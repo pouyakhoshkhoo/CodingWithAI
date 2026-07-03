45b4329043f6a75f45239282d8f8c4d9a54422f2
pouya khoshkhoo <p_aria2006@yahoo.com>
2026-07-03 23:38:37 +0330

    Wire map listing endpoint

--- backend/src/listings/api.ts
+++ backend/src/listings/api.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ListingsService } from './listings.service';
 
 @Controller('listings')
 export class ListingsController {
+  constructor(private readonly service: ListingsService) {}
+
   @Get()
   list() {
-    return [];
+    return this.service.listPublished();
+  }
+
+  @Get('map')
+  map(@Query() query: Record<string, string>) {
+    return this.service.mapQuery(Number(query.swLat), Number(query.swLng), Number(query.neLat), Number(query.neLng));
   }
 }
